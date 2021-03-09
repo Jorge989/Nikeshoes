158caca002b7f17f206af2eb8dc28abac15f0c21
Jorge989 <attiejorge89@gmail.com>
2021-03-09 15:41:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import nike1 from "./assets/run2.png";
+import nike2 from "./assets/run5.png";
+import nike3 from "./assets/run7.png";
+import nike4 from "./assets/runnew.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <section>
+      <div className="container">
+        <div className="card">
+          <div className="imgBx">
+          <img src={nike1} alt="nike"></img>
+          <h2>Nike Shoes</h2>
+          </div>
+          <div className="content">
+            <div className="size">
+<h3>Size :</h3>
+<span>7</span>
+<span>8</span>
+<span>9</span>
+<span>10</span>
+
+</div>
+<div className="color">
+<h3>Color :</h3>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+
+</div>
+<a href="/about">Buy Now</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBx">
+          <img src={nike2} alt="nike"></img>
+          <h2 className="nike2">Nike Shoes</h2>
+          </div>
+          <div className="content">
+            <div className="size">
+<h3>Size :</h3>
+<span>7</span>
+<span>8</span>
+<span>9</span>
+<span>10</span>
+
+</div>
+<div className="color">
+<h3>Color :</h3>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+
+</div>
+<a href="/about">Buy Now</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBx">
+          <img src={nike4} alt="nike"></img>
+          <h2 className="nike3">Nike Shoes</h2>
+          </div>
+          <div className="content">
+            <div className="size">
+<h3>Size :</h3>
+<span>7</span>
+<span>8</span>
+<span>9</span>
+<span>10</span>
+
+</div>
+<div className="color">
+<h3>Color :</h3>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+
+</div>
+<a href="/about">Buy Now</a>
+          </div>
+        </div>
+      </div>
+      </section>
+    </body>
   );
 }
 
